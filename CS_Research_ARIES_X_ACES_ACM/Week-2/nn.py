bdf60004a00e6caea9225246073ceaf9a95f14c5
reddragonnm <neeleshojmishra@gmail.com>
2025-06-09 20:55:00 +0530

    Make neural network

---
+++ CS_Research_ARIES_X_ACES_ACM/Week-2/nn.py
@@ -0,0 +1,95 @@
+import numpy as np
+
+
+def linear(x):
+    return x
+
+
+def linear_dash(x):
+    return 1
+
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+
+def sigmoid_dash(x):
+    sig = sigmoid(x)
+    return sig * (1 - sig)
+
+
+def relu(x):
+    return np.maximum(0, x)
+
+
+def relu_dash(x):
+    return int(x > 0)
+
+
+class Layer:
+    def __init__(
+        self, inp_size, out_size, activation=linear, activation_dash=linear_dash
+    ):
+        self.inp_size = inp_size
+        self.out_size = out_size
+
+        self.weights = np.random.randn(inp_size, out_size)
+        self.bias = np.random.randn(out_size)
+
+        self.activation = activation
+        self.activation_dash = activation_dash
+        self.inp = None
+
+    def feed(self, inp):
+        self.inp = inp
+        return self.activation(self.weights.T @ inp + self.bias)
+
+    def backprop(self, gradient_till_now, lr):
+        assert self.inp is not None
+
+        delta = gradient_till_now * self.activation_dash(
+            self.weights.T @ self.inp + self.bias
+        )
+
+        self.weights -= lr * np.outer(self.inp, delta)
+        self.bias -= lr * delta
+
+        self.inp = None
+
+        return self.weights @ delta
+
+
+def predict(model, x):
+    y_pred = x.copy()
+    for layer in model:
+        y_pred = layer.feed(y_pred)
+    return y_pred
+
+
+def mse_loss(y_true, y_pred):
+    return np.mean((y_true - y_pred) ** 2)
+
+
+if __name__ == "__main__":
+    model = [
+        Layer(2, 3, activation=sigmoid, activation_dash=sigmoid_dash),
+        Layer(3, 1, activation=linear, activation_dash=linear_dash),
+    ]
+
+    x = np.array([1, 2])
+    y = np.array([3])
+
+    y_pred = predict(model, x)
+
+    for _ in range(100):
+        y_pred = predict(model, x)
+        loss = mse_loss(y, y_pred)
+
+        gradient = 2 * (y_pred - y) / len(y)
+
+        for layer in reversed(model):
+            gradient = layer.backprop(gradient, lr=0.01)
+
+        print(loss)
+
+    print(y_pred)
